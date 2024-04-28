dbd759a19853b695df7dcd75eb6932cc7f3b8005
Xiaojun1023 <gongxiaojun20021023@gmail.com>
2024-04-28 12:10:13 -0700

    contact page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -7,3 +7,7 @@ module.exports.loadIndexPage = async function(req, res) {
 exports.loadAboutPage = function(req, res) {
     res.render('about', { title: 'About Us' }); // Make sure 'about' matches the name of your EJS file in views
 };
+
+exports.loadContactPage = function(req, res) {
+    res.render('contact', { title: 'Contact Us' });  // Assuming 'contact' is the name of your EJS template for the contact page
+};
