91abba20400ccf997e5d413831c8864d610effd3
ellmaer <ellmaer2003@gmail.com>
2024-04-27 19:10:48 -0700

    Added the components for index.ejs to be displayed

---
+++ routes/indexRouter.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+//to process data sent in on request need body-parser module
+var bodyParser = require('body-parser');
+var path = require ('path'); //to work with separtors on any OS including Windows
+var querystring = require('querystring'); //for use in GET Query string of form URI/path?name=value
+
+router.use(bodyParser.json()); // for parsing application/json
+
+router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+var indexController = require("../controllers/indexController.js");
+
+router.get("/", indexController.loadIndexPage);
+
+module.exports = router;
