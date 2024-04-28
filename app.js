408246d8a7837a3215e0a9070c548f5f2f5af16d
ellmaer <ellmaer2003@gmail.com>
2024-04-27 18:18:16 -0700

    This is our main update, final one

--- app.js
+++ app.js
@@ -1,94 +1,42 @@
 // Import necessary modules
-
 var createError = require('http-errors');
-
 var express = require('express');
-
 var session = require('express-session');
-
 var path = require('path');
-
 var cookieParser = require('cookie-parser');
-
 var logger = require('morgan');
-
-
-
 
 const app = express();
 
-
-
-
+// Set the views directory and view engine
 app.set('views', path.join(__dirname, 'views'));
-
 app.set('view engine', 'ejs');
 
-
-
-
+// Middleware
 app.use(logger('dev')); // can remove later
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: false }));
-
 app.use(cookieParser());
 
-
-
-
-const publicDirectory = path.join(__dirname, 'public');
-
-
-
-
-// Serve static files from the public directory
-
-app.use(express.static(publicDirectory));
-
-
-
+// Define routes
+app.get('/', function(req, res, next) {
+    res.render('index');
+});
 
 // catch 404 and forward to error handler
-
 app.use(function(req, res, next) {
-
     next(createError(404));
-
 });
-
-
-
 
 // error handler
-
 app.use(function(err, req, res, next) {
-
     // set locals, only providing error in development
-
     res.locals.message = err.message;
-
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-
-
-
     // render the error page
-
     res.status(err.status || 500);
-
     res.render('error');
-
 });
-
-
-
-
-
-
-
-
-
 
 module.exports = app;
